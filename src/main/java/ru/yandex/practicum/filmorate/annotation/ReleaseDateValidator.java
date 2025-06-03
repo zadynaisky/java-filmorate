4d4ea0df6d04173593aacbbd5878365fa9a15019
Ostap Zadunaisky <ezakharoffdrive@yandex.ru>
2025-06-03 21:03:49 +0300

    METHOD_DEF separated from previous line

--- src/main/java/ru/yandex/practicum/filmorate/annotation/ReleaseDateValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/annotation/ReleaseDateValidator.java
@@ -7,6 +7,7 @@ import java.time.LocalDate;
 
 public class ReleaseDateValidator implements ConstraintValidator<ReleaseDate, LocalDate> {
     private static final LocalDate DATE_1895_12_28 = LocalDate.of(1895, 12, 28);
+
     @Override
     public boolean isValid(LocalDate date, ConstraintValidatorContext constraintValidatorContext) {
         if (date == null || DATE_1895_12_28.isAfter(date))
