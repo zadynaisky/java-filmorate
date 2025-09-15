4fa6d587a06821836bf202d48182e486e78a598a
Ostap Zadunaisky <ezakharoffdrive@yandex.ru>
2025-09-15 22:56:15 +0300

    Changed citation rules

--- src/main/java/ru/yandex/practicum/filmorate/storage/repository/ReviewRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/repository/ReviewRepository.java
@@ -19,22 +19,22 @@ public class ReviewRepository extends BaseRepository<Review> {
     }
 
     public Collection<Review> findAll(int count) {
-        String sql = "SELECT * FROM review ORDER BY id DESC LIMIT ?;";
+        String sql = "SELECT * FROM \"REVIEW\" ORDER BY id DESC LIMIT ?;";
         return findMany(sql, reviewRowMapper, count);
     }
 
     public Collection<Review> findAllByFilmId(long filmId, int count) {
-        String sql = "SELECT * FROM review WHERE film_id = ? ORDER BY id DESC LIMIT ?;";
+        String sql = "SELECT * FROM \"REVIEW\" WHERE film_id = ? ORDER BY id DESC LIMIT ?;";
         return findMany(sql, reviewRowMapper, filmId, count);
     }
 
     public Review findById(long id) {
-        String sql = "SELECT * FROM review WHERE id = ?;";
+        String sql = "SELECT * FROM \"REVIEW\" WHERE id = ?;";
         return findOne(sql, reviewRowMapper, id);
     }
 
     public Review create(Review review) {
-        String sql = "INSERT INTO review (content, is_positive, film_id, user_id) VALUES (?, ?, ?, ?);";
+        String sql = "INSERT INTO \"REVIEW\" (content, is_positive, film_id, user_id) VALUES (?, ?, ?, ?);";
         var id = insert(sql,
                 review.getContent(),
                 review.getIsPositive(),
@@ -45,25 +45,25 @@ public class ReviewRepository extends BaseRepository<Review> {
     }
 
     public Review update(Review review) {
-        String sql = "UPDATE review SET content = ?, is_positive = ?, film_id = ?, user_id = ? WHERE id = ?;";
+        String sql = "UPDATE \"REVIEW\" SET content = ?, is_positive = ?, film_id = ?, user_id = ? WHERE id = ?;";
         update(sql, review.getContent(), review.getIsPositive(), review.getFilmId(), review.getUserId(), review.getId());
         return review;
     }
 
     public boolean delete(long id) {
-        String sql = "DELETE FROM review WHERE id = ?;";
+        String sql = "DELETE FROM \"REVIEW\" WHERE id = ?;";
         return delete(sql, id);
     }
 
     public void addLikeOrDislike(final Long reviewId, final Long userId, final boolean isLike) {
-        String sql = "MERGE INTO review_rating (review_id, user_id, is_positive) VALUES (?, ?, ?)";
+        String sql = "MERGE INTO \"REVIEW_RATING\" (review_id, user_id, is_positive) VALUES (?, ?, ?)";
         log.info("Adding like or dislike to review {} and user {}", reviewId, userId);
         insertMultipleKeys(sql, reviewId, userId, isLike);
         updateUseful(reviewId);
     }
 
     public void removeLikeOrDislike(final Long reviewId, final Long userId) {
-        String sql = "DELETE FROM review_rating WHERE review_id = ? AND user_id = ?";
+        String sql = "DELETE FROM \"REVIEW_RATING\" WHERE review_id = ? AND user_id = ?";
         log.info("Removing like  or dislike from review {} and user {}", reviewId, userId);
         delete(sql, reviewId, userId);
         updateUseful(reviewId);
@@ -71,14 +71,14 @@ public class ReviewRepository extends BaseRepository<Review> {
 
     public void updateUseful(long reviewId) {
         String sql = """
-                UPDATE review
+                UPDATE \"REVIEW\"
                 SET useful =
                         (SELECT SUM(CASE
                                 WHEN is_positive = TRUE THEN 1
                                 WHEN is_positive = FALSE THEN -1
                                 ELSE 0
-                                END) AS film_rating
-                        FROM review_rating
+                                END)
+                        FROM \"REVIEW_RATING\"
                         WHERE review_id = ?)
                     WHERE id = ?;
                 """;
