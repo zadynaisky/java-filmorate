87dc8ead11f80372d6b8e21de290ef7c8d8bc437
German-Trosko <skodagrodno2006@gmail.com>
2025-09-17 17:30:17 +0300

    43

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -9,7 +9,6 @@ import ru.yandex.practicum.filmorate.service.RecommendationService;
 import ru.yandex.practicum.filmorate.service.UserService;
 
 import java.util.Collection;
-import java.util.List;
 
 @RestController
 @RequestMapping("/users")
