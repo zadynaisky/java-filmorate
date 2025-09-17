9f6e5c900d6c80349013b34985993d65f806d273
German-Trosko <skodagrodno2006@gmail.com>
2025-09-17 10:11:56 +0300

    movie recommendation system 11

--- src/main/java/ru/yandex/practicum/filmorate/service/RecommendationService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/RecommendationService.java
@@ -56,7 +56,7 @@ public class RecommendationService {
 
             // Получаем ID рекомендованных фильмов
             List<Long> recommendedFilmIds = recommendationRepository.getRecommendedFilmIds(userId, similarUserId);
-            
+
             // Если нет рекомендаций от самого похожего пользователя, попробуем найти любые фильмы, которые не лайкнул текущий пользователь
             if (recommendedFilmIds.isEmpty()) {
                 return Collections.emptyList();
