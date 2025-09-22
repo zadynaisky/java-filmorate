ef4ff1dd3ce0bb1d0dc19aa9413a53f40d7f0839
German-Trosko <skodagrodno2006@gmail.com>
2025-09-22 09:05:49 +0300

    исправил 22.09

--- src/main/java/ru/yandex/practicum/filmorate/storage/repository/LikeRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/repository/LikeRepository.java
@@ -20,36 +20,4 @@ public class LikeRepository extends BaseRepository<Long> {
         String sql = "DELETE FROM \"LIKE\" WHERE user_id = ? AND film_id = ?";
         delete(sql, userId, filmId);
     }
-
-    /**
-     * Получить список фильмов, которые лайкнул пользователь
-     */
-    public Collection<Long> getLikedFilmsByUser(final Long userId) {
-        String sql = "SELECT film_id FROM \"LIKE\" WHERE user_id = ?";
-        return jdbcTemplate.queryForList(sql, Long.class, userId);
-    }
-
-    /**
-     * Получить список пользователей, которые лайкнули фильм
-     */
-    public Collection<Long> getUsersWhoLikedFilm(final Long filmId) {
-        String sql = "SELECT user_id FROM \"LIKE\" WHERE film_id = ?";
-        return jdbcTemplate.queryForList(sql, Long.class, filmId);
-    }
-
-    /**
-     * Получить всех пользователей с их лайками (если где-то потребуется)
-     */
-    public Map<Long, Set<Long>> getAllUsersLikes() {
-        String sql = "SELECT user_id, film_id FROM \"LIKE\"";
-        Map<Long, Set<Long>> userLikes = new HashMap<>();
-
-        jdbcTemplate.query(sql, rs -> {
-            Long userId = rs.getLong("user_id");
-            Long filmId = rs.getLong("film_id");
-            userLikes.computeIfAbsent(userId, k -> new HashSet<>()).add(filmId);
-        });
-
-        return userLikes;
-    }
 }
