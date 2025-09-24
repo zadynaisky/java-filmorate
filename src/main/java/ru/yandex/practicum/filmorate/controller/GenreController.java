6d3c2bde08b62cd182f7e91d43a8ebb314828fe8
German-Trosko <skodagrodno2006@gmail.com>
2025-09-24 23:31:09 +0300

    7

--- src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
@@ -16,7 +16,7 @@ import java.util.Collection;
 public class GenreController {
 
     private final GenreService genreService;
-    
+
     @GetMapping
     public Collection<Genre> findAll() {
         return genreService.findAll();
